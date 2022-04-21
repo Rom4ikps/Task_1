379e79f3a05441f513cff55da014638d5e285a75
Rom4ikps <96090636+Rom4ikps@users.noreply.github.com>
2022-04-21 13:24:18 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+
+	  function Resoult() {
+
+	    var x = Number(document.getElementById("x").value);
+	    var y = Number(document.getElementById("y").value);
+	    var c = x + y;
+	    console.log((c));
+
+	  }
